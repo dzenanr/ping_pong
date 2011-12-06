88cf406ae3ee889b6a363c5dd59ce4bc33b1e6fe
Dzenan Ridjanovic <dzenanr@gmail.com>
2011-12-06 16:23:41 -0500

    move

--- Board.dart
+++ Board.dart
@@ -1,25 +1,16 @@
 class Board {
   CanvasRenderingContext2D context;
   
+  int width;
+  int height;
+  
   Board(CanvasElement canvas) {
     context = canvas.getContext("2d");
+    width = canvas.width;
+    height = canvas.height;
   }
   
-  //draw a circle
-  void circle(x, y, r, style) {
-    // context.setStrokeStyle(style);  // bug: nothing drawn
-    context.beginPath();
-    context.arc(x, y, r, 0, Math.PI*2, true);
-    context.closePath();
-    context.stroke();
-  }
-    
-  //draw a rectangle
-  void rectangle(x, y, w, h, style) {
-    //context.setFillStyle(style);  // bug: nothing drawn
-    context.beginPath();
-    context.rect(x,y,w,h);
-    context.closePath();
-    context.stroke();
-  }
+  void clear() {
+    context.clearRect(0, 0, width, height);
+  } 
 }
--- pingpong.dart
+++ pingpong.dart
@@ -1,12 +1,12 @@
 #import('dart:html');
 #source('Board.dart');
+#source('Ball.dart');
 
 void main() {
   //get a reference to the canvas
   CanvasElement canvas = document.query('#canvas');
   Board board = new Board(canvas);
-  board.circle(220, 220, 50, "#00A308");
-  board.circle(100, 100, 100, "#FF1C0A");
-  board.rectangle(15, 150, 120, 120, "rgba(255, 255, 0, .5)");
-  board.rectangle(150, 250, 220, 100, "rgba(355, 355, 0, .75)");
+  Ball ball = new Ball(board, 0, 0, 10);
+  //call the function move() every 10ms.
+  document.window.setInterval(ball.move, 10);
 }
